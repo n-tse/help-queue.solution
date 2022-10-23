e78661d0ff0a46c976ca1738369e326368fe94a2
Nick Tse <nicholashtse@gmail.com>
2022-10-23 16:28:24 -0700

    replace form placeholder in newticketform component with an actual form

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -3,7 +3,20 @@ import React from "react";
 function NewTicketForm(props){
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
