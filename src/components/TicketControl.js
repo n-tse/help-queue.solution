c726bd39b0fa88aba51cea77b8feb4c1a5f363e4
Nick Tse <nicholashtse@gmail.com>
2022-10-29 19:08:58 -0700

    pass handledeletingticket as prop to ticketdetail

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -53,7 +53,7 @@ class TicketControl extends React.Component {
     let buttonText = null; 
 
     if (this.state.selectedTicket != null) {
-      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} />
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} onClickingDelete = {this.handleDeletingTicket} />
       buttonText = "Return to Ticket List";
     }
     else if (this.state.formVisibleOnPage) {
